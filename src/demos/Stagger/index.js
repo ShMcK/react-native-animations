1f0a796da7aca794e5a423d0cb48ca6cb63f7cd1
ShMcK <shawn.j.mckay@gmail.com>
2016-12-07 15:30:39 -0800

    update stagger demo

--- src/demos/Stagger/index.js
+++ src/demos/Stagger/index.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react'
-import { View, Animated, Dimensions } from 'react-native'
+import { ListView, Text, View, Animated, Dimensions } from 'react-native'
 import styles from './styles'
 
 const { height } = Dimensions.get('window')
-
-const dropAnimation = {
-  delay: 300,
-  toValue: height,
-  duration: 3000,
-}
 
 class StaggerDemo extends Component {
   static route = {
@@ -17,30 +11,50 @@ class StaggerDemo extends Component {
     }
   }
   componentWillMount() {
-    this.animatedValue1 = new Animated.Value(0)
-    this.animatedValue2 = new Animated.Value(0)
-    this.animatedValue3 = new Animated.Value(0)
+    this.animationList = []
+    const dropAnimation = {
+      delay: 100,
+      toValue: height,
+      duration: 2000,
+    }
+    this.rows = [
+      { title: 'First', color: 'blue' },
+      { title: 'Second', color: 'red' },
+      { title: 'Third', color: 'green' },
+      { title: 'Fourth', color: 'orange' },
+      { title: 'Fifth', color: 'purple' },
+      { title: 'Sixth', color: 'pink' },
+      { title: 'Seventh', color: 'beige' },
+      { title: 'Eighth', color: 'indigo' },
+    ]
+
+    this.rows.forEach((r, i) => {
+      const name = 'av' + i
+      this[name] = new Animated.Value(0)
+      this['avStyle' + i] = { height: this[name] }
+      this.animationList.push(
+        Animated.timing(this[name], dropAnimation)
+      )
+    })
   }
   componentDidMount() {
-    Animated.stagger(1500, [
-      Animated.timing(this.animatedValue1, dropAnimation),
-      Animated.timing(this.animatedValue2, dropAnimation),
-      Animated.timing(this.animatedValue3, dropAnimation)
-    ]).start()
+    Animated.stagger(1500, this.animationList).start()
   }
   render() {
-    const animatedStyle1 = { height: this.animatedStyle1 }
-    const animatedStyle2 = { height: this.animatedValue2 }
-    const animatedStyle3 = { height: this.animatedValue3 }
     return (
-      <View style={styles.container}>
-        <Animated.View style={[styles.box, { backgroundColor: 'red' }, animatedStyle1]} />
-        <Animated.View style={[styles.box, { backgroundColor: 'green' }, animatedStyle2]} />
-        <Animated.View style={[styles.box, { backgroundColor: 'blue' }, animatedStyle3]} />
+      <View>
+        {this.rows.map((row, i) => (
+          <Animated.View key={i} style={[
+            styles.listItem,
+            this['avStyle' + i],
+            { backgroundColor: row.color }
+          ]}>
+            <Text style={styles.text}>{row.title}</Text>
+          </Animated.View>
+        ))}
       </View>
     )
   }
 }
 
 export default StaggerDemo
-
--- src/demos/Stagger/styles.js
+++ src/demos/Stagger/styles.js
@@ -3,15 +3,32 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
   box: {
     flex: 1,
     backgroundColor: '#333',
     marginHorizontal: 5,
   },
+  listItem: {
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxHeight: 35,
+    borderRadius: 8,
+    margin: 20,
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowRadius: 8,
+    shadowColor: 'grey',
+    shadowOpacity: 0.5,
+  },
   text: {
-    color: 'white'
+    color: 'white',
+    fontWeight: 'bold',
   }
 })
 
