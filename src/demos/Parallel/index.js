fc5769364eab15de8a8cec311d18c469c309872b
ShMcK <shawn.j.mckay@gmail.com>
2016-12-07 14:55:32 -0800

    update parallel example

--- src/demos/Parallel/index.js
+++ src/demos/Parallel/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Animated } from 'react-native'
+import { View, Text, Animated, TouchableHighlight } from 'react-native'
 import styles from './styles'
 
 class ParallelDemo extends Component {
@@ -9,32 +9,80 @@ class ParallelDemo extends Component {
     }
   }
   componentWillMount() {
-    this.animatedValue1 = new Animated.Value(0)
-    this.animatedValue2 = new Animated.Value(1)
+    this.avY = new Animated.Value(0)
+    this.avScale = new Animated.Value(1)
+    this.avColor = new Animated.Value(0)
   }
   componentDidMount() {
+    this.position = 0
+    this.animate()
+  }
+  animate() {
+    if (this.position === 1) {
+      this.animateUp()
+    } else if (this.position === 0) {
+      this.animateDown()
+    }
+  }
+  animateDown() {
+    this.position = 1
     Animated.parallel([
-      Animated.timing(this.animatedValue1, {
+      Animated.timing(this.avY, {
         delay: 800,
         toValue: 300,
         duration: 2000,
       }),
-      Animated.spring(this.animatedValue2, {
+      Animated.spring(this.avScale, {
+        delay: 800,
         toValue: 3,
         duration: 2000,
+      }),
+      Animated.timing(this.avColor, {
+        toValue: 120,
+        duration: 2000
+      })
+    ]).start()
+  }
+  animateUp() {
+    this.position = 0
+    Animated.parallel([
+      Animated.timing(this.avY, {
+        delay: 800,
+        toValue: 0,
+        duration: 2000,
+      }),
+      Animated.spring(this.avScale, {
+        delay: 800,
+        toValue: 1,
+        duration: 2000,
+      }),
+      Animated.timing(this.avColor, {
+        toValue: 0,
+        duration: 2000
       })
     ]).start()
   }
   render() {
+    const interpolateColor = this.avColor.interpolate({
+      inputRange: [0, 150],
+      outputRange: ['rgb(0, 0, 0)', 'rgb(51, 250, 170)',]
+    })
     const animateStyles = {
+      backgroundColor: interpolateColor,
       transform: [{
-        translateY: this.animatedValue1
+        translateY: this.avY
       }, {
-        scale: this.animatedValue2
+        scale: this.avScale
       }]
     }
     return (
       <View style={styles.container} >
+        <TouchableHighlight style={styles.button} onPress={this.animate.bind(this)}>
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableHighlight>
+
+        <Text style={styles.text}>Run multiple animations in parallel</Text>
+
         <Animated.View style={[styles.box, animateStyles]} />
       </View>
     )
--- src/demos/Parallel/styles.js
+++ src/demos/Parallel/styles.js
@@ -3,16 +3,29 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   box: {
     height: 100,
     width: 100,
     backgroundColor: 'black'
   },
-  text: {
+  button: {
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 35,
+    borderRadius: 8,
+  },
+  buttonText: {
     color: 'white'
+  },
+  text: {
+    marginTop: 20,
+    marginBottom: 20,
   }
 })
 
